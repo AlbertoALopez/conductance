ae08a2314018dea70e4635f4a6643f4e4d42ae82
Alberto Lopez <albertolopez19@gmail.com>
2018-02-10 12:44:37 -0500

    Add basic graphql server to express

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const next = require('next');
+const { buildSchema } = require('graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello World!';
+  },
+};
+
 app.prepare()
   .then(() => {
     const server = express();
+
+    server.use('/graphql', graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+    }));
 
     server.get('*', (req, res) => {
       return handle(req, res);
