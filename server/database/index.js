bb68d461044c22414d619d8a276bcccf69f52529
Alberto Lopez <albertolopez19@gmail.com>
2018-02-13 14:39:20 -0500

    Change .env fields

--- server/database/index.js
+++ server/database/index.js
@@ -2,14 +2,13 @@ const mysql = require('mysql');
 
 const mysqlPool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.GCP_SQL_HOST,
-  user: process.env.GCP_SQL_USER,
-  password: process.env.GCP_SQL_PASSWORD,
-  localAddress: process.env.GCP_SQL_LOCAL_ADDRESS,
-  port: process.env.GCP_SQL_PORT,
-  database: process.env.GCP_SQL_DB_NAME,
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  localAddress: process.env.SQL_LOCAL_ADDRESS,
+  port: process.env.SQL_PORT,
+  database: process.env.SQL_DB_NAME,
 });
-
 
 module.exports = {
   mysqlPool,
